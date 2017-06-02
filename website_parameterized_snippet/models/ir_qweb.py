c9519f0b00a4cb40042d5f0651c893770250c216
Nemry Jonathan <jonathan.nemry@acsone.eu>
2017-06-02 09:32:59 +0200

    [MIG] 10.0 website_parametrized_snippet

--- website_parameterized_snippet/models/ir_qweb.py
+++ website_parameterized_snippet/models/ir_qweb.py
@@ -2,7 +2,7 @@
 # (C) 2015 Therp BV <http://therp.nl>.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import models
+from odoo import models
 from lxml import html
 from lxml import etree
 
